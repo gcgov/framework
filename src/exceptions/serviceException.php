d578977340df3560359d3f05573ed6935b04429b
Andrew Sauder <andrew@sauder.email>
2021-07-27 16:48:37 -0400

    add service exception type

---
+++ src/exceptions/serviceException.php
@@ -0,0 +1,17 @@
+<?php
+namespace gcgov\framework\exceptions;
+
+
+class serviceException
+	extends
+	\LogicException {
+
+	// Redefine the exception so message isn't optional
+
+	/** @noinspection PhpPureAttributeCanBeAddedInspection */
+	public function __construct( $message, $code = 0, \Exception $previous = null ) {
+		// make sure everything is assigned properly
+		parent::__construct( $message, $code, $previous );
+	}
+
+}
