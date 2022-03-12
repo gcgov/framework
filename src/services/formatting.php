132d99316d6eb11b803382b9a1816c1f5757e9cf
Andrew Sauder <andrewsauder@gmail.com>
2022-03-12 02:46:34 -0500

    add excel tab naming formatter

--- src/services/formatting.php
+++ src/services/formatting.php
@@ -5,7 +5,14 @@ namespace gcgov\framework\services;
 
 class formatting {
 
-	public static function fileName( string $fileName, string $replacementForIllegalChars='-' ) {
+	/**
+	 * @param  string  $fileName
+	 * @param  string  $replacementForIllegalChars
+	 * @param  bool    $forceLowerCase
+	 *
+	 * @return string
+	 */
+	public static function fileName( string $fileName, string $replacementForIllegalChars='-', bool $forceLowerCase=true ) : string {
 		$illegalChars = [
 			'\\',
 			'/',
@@ -16,9 +23,38 @@ class formatting {
 			'<',
 			'>',
 			'|',
+			',',
 			' '
 		];
-		return str_replace( $illegalChars, '-', strtolower($fileName) );
+
+		$correction1 = $forceLowerCase ? strtolower($fileName) : $fileName;
+		$correction2 = str_replace( $illegalChars, $replacementForIllegalChars, $correction1 );
+		$correction3 = preg_replace('/('.$replacementForIllegalChars.')+/', ' ', $correction2);
+		return $correction3;
+	}
+
+
+	/**
+	 * @param  string  $tabName
+	 * @param  string  $replacementForIllegalChars
+	 * @param  bool    $forceLowerCase
+	 *
+	 * @return string
+	 */
+	public static function xlsxTabName( string $tabName, string $replacementForIllegalChars=' ', bool $forceLowerCase=false ) : string {
+		$illegalChars = [
+			'\\',
+			'/',
+			'*',
+			'[',
+			']',
+			':',
+			'?'
+		];
+		$correction1 = $forceLowerCase ? strtolower($tabName) : $tabName;
+		$correction2 = str_replace( $illegalChars, $replacementForIllegalChars, $correction1 );
+		$correction3 = preg_replace('/('.$replacementForIllegalChars.')+/', ' ', $correction2);
+		return $correction3;
 	}
 
 }
