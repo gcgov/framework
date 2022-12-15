671b3acbe3767860503fd740ec5362a367f983f1
Andrew Sauder <andrewsauder@gmail.com>
2022-12-15 11:22:44 -0500

    code formatting

--- src/services/log.php
+++ src/services/log.php
@@ -1,56 +1,54 @@
 <?php
 namespace gcgov\framework\services;
 
-
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-
 final class log {
 
-	public static function debug( string $channel, string $message, array $context = [] ) {
+	public static function debug( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->debug( $message, $context );
 	}
 
 
-	public static function info( string $channel, string $message, array $context = [] ) {
+	public static function info( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->info( $message, $context );
 	}
 
 
-	public static function notice( string $channel, string $message, array $context = [] ) {
+	public static function notice( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->notice( $message, $context );
 	}
 
 
-	public static function warning( string $channel, string $message, array $context = [] ) {
+	public static function warning( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->warning( $message, $context );
 	}
 
 
-	public static function error( string $channel, string $message, array $context = [] ) {
+	public static function error( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->error( $message, $context );
 	}
 
 
-	public static function critical( string $channel, string $message, array $context = [] ) {
+	public static function critical( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->critical( $message, $context );
 	}
 
 
-	public static function alert( string $channel, string $message, array $context = [] ) {
+	public static function alert( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->alert( $message, $context );
 	}
 
 
-	public static function emergency( string $channel, string $message, array $context = [] ) {
+	public static function emergency( string $channel, string $message, array $context = [] ): void {
 		$logger = self::getLogger( $channel );
 		$logger->emergency( $message, $context );
 	}
@@ -58,6 +56,7 @@ final class log {
 
 	/** @var Logger[] */
 	private static array $loggers = [];
+
 
 	private static function getLogger( string $channel = '' ): Logger {
 		if( isset( self::$loggers[ $channel ] ) ) {
