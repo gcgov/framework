7471216fda696dfeafc93e7e0bef1484bf6da976
Andrew Sauder <andrewsauder@gmail.com>
2022-12-15 11:23:05 -0500

    add start of view controller response handling

--- src/renderer.php
+++ src/renderer.php
@@ -2,7 +2,6 @@
 
 namespace gcgov\framework;
 
-
 use gcgov\framework\exceptions\controllerException;
 use gcgov\framework\models\routeHandler;
 use gcgov\framework\interfaces\controller;
@@ -11,7 +10,6 @@ use gcgov\framework\models\controllerViewResponse;
 use gcgov\framework\exceptions\modelException;
 use gcgov\framework\exceptions\routeException;
 
-
 final class renderer {
 
 	public function __construct() {
@@ -19,11 +17,11 @@ final class renderer {
 
 
 	/**
-	 * @param  \gcgov\framework\models\routeHandler|\gcgov\framework\exceptions\routeException  $routeHandlerOrException
+	 * @param \gcgov\framework\models\routeHandler|\gcgov\framework\exceptions\routeException $routeHandlerOrException
 	 *
 	 * @return string
 	 */
-	public function render( routeHandler|routeException $routeHandlerOrException ) : string {
+	public function render( routeHandler|routeException $routeHandlerOrException ): string {
 		if( $routeHandlerOrException instanceof routeHandler ) {
 			$controllerResponse = $this->getContentFromController( $routeHandlerOrException );
 		}
@@ -38,10 +36,12 @@ final class renderer {
 		elseif( $controllerResponse instanceof controllerViewResponse ) {
 			return $this->processControllerViewResponse( $controllerResponse );
 		}
+
+		return '';
 	}
 
 
-	private function getContentFromController( routeHandler $routeHandler ) : controllerDataResponse|controllerViewResponse {
+	private function getContentFromController( routeHandler $routeHandler ): controllerDataResponse|controllerViewResponse {
 		//exceptions raised here are all logic exceptions: a route is defined that points to a controllerClass or method that does not exist
 		try {
 			$controllerClass = new \ReflectionClass( $routeHandler->class );
@@ -79,16 +79,16 @@ final class renderer {
 
 				return \app\renderer::processControllerException( $e );
 			}
-			catch( \Exception | \Error | \ErrorException $e ) {
+			catch( \Exception|\Error|\ErrorException $e ) {
 				\error_log( $e );
-				\gcgov\framework\services\log::error( 'Renderer', $e->getMessage(), [$e] );
+				\gcgov\framework\services\log::error( 'Renderer', $e->getMessage(), [ $e ] );
 
 				return \app\renderer::processSystemErrorException( $e );
 			}
 		}
 		catch( \ReflectionException $e ) {
 			error_log( $e );
-			\gcgov\framework\services\log::error( 'Renderer', $e->getMessage(), [$e] );
+			\gcgov\framework\services\log::error( 'Renderer', $e->getMessage(), [ $e ] );
 
 			return \app\renderer::processSystemErrorException( $e );
 		}
@@ -96,33 +96,33 @@ final class renderer {
 
 
 	/**
-	 * @param  \gcgov\framework\models\controllerDataResponse  $controllerDataResponse
+	 * @param \gcgov\framework\models\controllerDataResponse $controllerDataResponse
 	 *
 	 * @return string
 	 */
-	private function processControllerDataResponse( controllerDataResponse $controllerDataResponse ) : string {
-		if (!headers_sent($filename, $lineNumber)) {
-			header( 'Content-Type:'.$controllerDataResponse->getContentType() );
-		} else {
-			\gcgov\framework\services\log::debug( 'Renderer', 'Cannot set content-type header. Headers already sent in '.$filename.' on line '.$lineNumber );
+	private function processControllerDataResponse( controllerDataResponse $controllerDataResponse ): string {
+		if( !headers_sent( $filename, $lineNumber ) ) {
+			header( 'Content-Type:' . $controllerDataResponse->getContentType() );
+		}
+		else {
+			\gcgov\framework\services\log::debug( 'Renderer', 'Cannot set content-type header. Headers already sent in ' . $filename . ' on line ' . $lineNumber );
 		}
 
-
-		if( $controllerDataResponse->getHttpStatus() != 200 ) {
+		if( $controllerDataResponse->getHttpStatus()!=200 ) {
 			http_response_code( $controllerDataResponse->getHttpStatus() );
-			if( $controllerDataResponse->getData() === null ) {
+			if( $controllerDataResponse->getData()===null ) {
 				return '';
 			}
 		}
 
 		if( $controllerDataResponse->getContentType()==='application/json' ) {
 			$encodedResponse = json_encode( $controllerDataResponse->getData() );
-			if( $encodedResponse === false ) {
+			if( $encodedResponse===false ) {
 				return \app\renderer::processSystemErrorException( new \LogicException( 'JSON encoding of controller->data failed', 0 ) );
 			}
 		}
 		elseif( $controllerDataResponse->getContentType()==='text/plain' ) {
-			$encodedResponse = (string) $controllerDataResponse->getData();
+			$encodedResponse = (string)$controllerDataResponse->getData();
 		}
 		else {
 			return \app\renderer::processSystemErrorException( new \LogicException( 'Unsupported content-type provided in controller response', 500 ) );
@@ -132,18 +132,31 @@ final class renderer {
 	}
 
 
-	private function processControllerViewResponse( controllerViewResponse $controllerViewResponse ) : string {
-		//LOGIC ERROR: view and vars for templated views does not exist
-		throw new \LogicException( 'View and vars not implemented' );
+	private function processControllerViewResponse( controllerViewResponse $controllerViewResponse ): string {
+		$viewFile      = $controllerViewResponse->getView();
+		$varsToHydrate = $controllerViewResponse->getVars();
+		try {
+			ob_start();
+			foreach( $varsToHydrate as $key => $value ) {
+				${$key} = $value;
+			}
+			include_once( $viewFile );
+			$content = ob_get_contents();
+			ob_end_clean();
+		}
+		catch( \Throwable $e ) {
+			return \app\renderer::processSystemErrorException( $e );
+		}
+		return $content;
 	}
 
 
 	/**
-	 * @param  \ReflectionClass  $controllerClass
+	 * @param \ReflectionClass $controllerClass
 	 *
 	 * @throws \ReflectionException
 	 */
-	private function lifecycleControllerBefore( \ReflectionClass $controllerClass ) {
+	private function lifecycleControllerBefore( \ReflectionClass $controllerClass ): void {
 		$controllerInstance = $controllerClass->newInstanceWithoutConstructor();
 
 		$_beforeMethod = $controllerClass->getMethod( '_before' );
@@ -153,10 +166,10 @@ final class renderer {
 
 
 	/**
-	 * @param  controller  $controllerInstance
+	 * @param controller $controllerInstance
 	 *
 	 */
-	private function lifecycleControllerAfter( controller $controllerInstance ) {
+	private function lifecycleControllerAfter( controller $controllerInstance ): void {
 		$controllerInstance::_after();
 	}
 
