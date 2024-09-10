a081e7556bc39ab2eea3d170175a6387a4f788b0
Andrew Sauder <andrew@sauder.email>
2024-09-10 10:02:48 -0400

    fix route handler allow short lived url tokens

--- src/models/routeHandler.php
+++ src/models/routeHandler.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace gcgov\framework\models;
-
 
 /**
  * Model for production and consumption by \gcgov\framework for acting upon \gcgov\framework\models\route
@@ -12,26 +10,27 @@ namespace gcgov\framework\models;
  */
 class routeHandler {
 
+	public string $class = '';
 
-	public string $class          = '';
+	public string $method = '';
 
-	public string $method         = '';
+	public bool $authentication = false;
 
-	public bool   $authentication = false;
-
-	public array  $arguments      = [];
+	public array $arguments = [];
 
 	/** @var string[] If authentication is required, the user must have all of these roles to get to route */
 	public array $requiredRoles = [];
 
-	public bool   $allowShortLivedUrlTokens = false;
+	public bool $allowShortLivedUrlTokens = false;
 
-	public function __construct( string $class, string $method, bool $authentication = false, array $requiredRoles=[], bool $allowShortLivedUrlTokens=false ) {
 
-		$this->class          = $class;
-		$this->method         = $method;
-		$this->authentication = $authentication;
-		$this->requiredRoles   = $requiredRoles;
+	public function __construct( string $class, string $method, bool $authentication = false, array $requiredRoles = [], bool $allowShortLivedUrlTokens = false ) {
+
+		$this->class                    = $class;
+		$this->method                   = $method;
+		$this->authentication           = $authentication;
+		$this->requiredRoles            = $requiredRoles;
+		$this->allowShortLivedUrlTokens = $allowShortLivedUrlTokens;
 
 	}
 
